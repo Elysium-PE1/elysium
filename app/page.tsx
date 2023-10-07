c034513cbf7ddf4e4487ede9e57e1619ce66ca9d
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-07 18:35:36 +0530

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { BookingForm } from "@/components/booking-form";
+import { LandingPage } from "@/components/landing-page";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      elysium
+    <main className="flex min-h-screen flex-col items-center pt-[60px] bg-[#010101] text-[#cabd89] overflow-x-hidden">
+      <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video 
+          src='/bg.mp4'
+          loop
+          muted
+          autoPlay         
+          className="object-cover w-fit object-center"
+        />
+      </div>
+      <Navbar />
+      <LandingPage />
+      <BookingForm />
     </main>
   );
 }
