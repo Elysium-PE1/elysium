747e0e04be12987ebee191784b43a882de2d07c1
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-27 14:05:53 +0530

    integrated custom hooks

--- components/booking-form.tsx
+++ components/booking-form.tsx
@@ -3,13 +3,50 @@
 import { useState } from "react";
 import { MessageBox } from "./message-box";
 import { useRouter } from "next/navigation";
+import { KnockAPI } from "@/lib/knockapi";
+import { KnockClient } from "@/lib/knockclient";
+
+interface FormInput {
+    name: string
+    email: string
+    phoneNo: string
+    resDate: string
+    resTime: string
+    noOfPeople: string
+    specialNotes: string
+}
 
 export const BookingForm = () => {
     const seats = [2, 3, 4, 5, 6, 7, 8];
     const router = useRouter()
+    const [userId, setUserId] = useState<string>('')
     const [showModel, setShowModel] = useState<boolean>(false)
+    const [formData, setFormData] = useState<FormInput>({
+        name: '',
+        email: '',
+        phoneNo: '',
+        resDate: '',
+        resTime: '',
+        noOfPeople: '',
+        specialNotes: '',
+    })
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target
+        setFormData((prev: FormInput) => ({
+            ...prev,
+            [name]: value,
+        }))
+        if (name == 'email')
+            setUserId(
+                formData.email.substring(0, formData.email.indexOf('@') + 1)
+            )
+    }
 
     const handleSubmit = () => {
+        KnockAPI.setIdentify(userId, formData)
+        KnockClient.getAuth(userId)
+        KnockAPI.triggerWorkflow(userId, 'book@elysium', formData)
         setTimeout(() => setShowModel(true), 500)
         setTimeout(() => {
             setShowModel(false)
@@ -22,7 +59,7 @@ export const BookingForm = () => {
         id="booking-form"
         className="relative w-screen h-screen bg-[#010101] flex flex-col justify-center items-center"
     >
-        <h1 className="text-5xl font-semibold tracking-wider">Booking Form</h1>
+        <h1 className="text-5xl mt-[60px] font-semibold tracking-wider">Booking Form</h1>
         <form
             onSubmit={(e: any) => {
                 e.preventDefault()
@@ -34,41 +71,36 @@ export const BookingForm = () => {
             {showModel && <MessageBox />}
             <div className="flex gap-[60px]">
             <div className="flex flex-col w-full">
-                <div className="mb-5">
-                <label className="text-lg font-medium mb-1">Name</label>
-                <div className="flex gap-3">
+                <div className="flex flex-col mb-5">
+                    <label className="text-lg font-medium mb-1">Name</label>
                     <input
-                    type="text"
-                    name="firstname"
-                    placeholder="First"
-                    className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
-                    />
-                    <input
-                    type="text"
-                    name="lastname"
-                    placeholder="Last"
-                    className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
-                    />
-                </div>
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
+                        onChange={handleChange}
+                        />
                 </div>
 
                 <div className="flex gap-3 mb-5">
-                <div className="flex flex-col">
-                    <label className="text-lg font-medium">Email</label>
-                    <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
-                    />
-                </div>
+                    <div className="flex flex-col">
+                        <label className="text-lg font-medium">Email</label>
+                        <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
+                        onChange={handleChange}
+                        />
+                    </div>
 
                 <div className="flex flex-col">
                     <label className="text-lg font-medium">Phone </label>
                     <input
                     type="tel"
-                    name="phone"
+                    name="phoneNo"
                     placeholder="Contact No"
+                    onChange={handleChange}
                     className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
                     />
                 </div>
@@ -79,8 +111,8 @@ export const BookingForm = () => {
                     <label className="text-lg font-medium">Reservation Date</label>
                     <input
                     type="date"
-                    name="reservationdate"
-                    placeholder="Email"
+                    name="resDate"
+                    onChange={handleChange}
                     className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
                     />
                 </div>
@@ -89,8 +121,8 @@ export const BookingForm = () => {
                     <label className="text-lg font-medium">Reservation Time</label>
                     <input
                     type="time"
-                    name="reservationtime"
-                    placeholder="Contact No"
+                    name="resTime"
+                    onChange={handleChange}
                     className="placeholder:text-[#010101] text-[#010101] p-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144]"
                     />
                 </div>
@@ -107,8 +139,9 @@ export const BookingForm = () => {
                     <label key={id}>
                         <input
                         type="radio"
-                        name="no-of-people"
+                        name="noOfPeople"
                         value={seat}
+                        onChange={handleChange}
                         className="hidden peer"
                         required
                         onClick={() => console.log()}
@@ -124,10 +157,11 @@ export const BookingForm = () => {
                 <div className="flex flex-col mb-5">
                 <label className="text-lg font-medium mb-1">Notes</label>
                 <textarea
-                    name="notes"
+                    name="specialNotes"
                     rows={5}
                     cols={50}
                     placeholder="Special Notes"
+                    onChange={handleChange}
                     className="placeholder:text-[#010101] w-[456px] h-full text-[#010101] py-1 px-2 bg-[#d1aa07] border-2 border-[#010101] outline outline-offset-2 outline-2 hover:outline-[#cbb144] focus:outline-[#cbb144] resize-none"
                 ></textarea>
                 </div>
